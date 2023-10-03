1be25c7d60f637a7cad193b2f6add61da9a91949
dsress11@gmail.com <dsress11@gmail.com>
2023-10-03 21:55:33 +0300

    fix: checked button on start design: books, example, main, menu, users fxml files feat: simple user add to db in UserController style: added styles to menu, tables and buttons

--- Library/lib-gui/src/main/java/ua/edu/dnu/project/MenuController.java
+++ Library/lib-gui/src/main/java/ua/edu/dnu/project/MenuController.java
@@ -1,14 +1,17 @@
 package ua.edu.dnu.project;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MenuController {
+
     public void openUsers(ActionEvent actionEvent) {
         MainPaneController.getInstance().setContent("users.fxml");
     }
